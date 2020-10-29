3f10745e6f120047f8ff47830bf3fc88adf56cb2
Misha <71318264+Matrix4555@users.noreply.github.com>
2020-10-29 13:01:16 +0900

    file updated to 1.02

--- trainingmode.h
+++ trainingmode.h
@@ -3,6 +3,7 @@
 
 #include <QDialog>
 #include <QMessageBox>
+#include <QTimer>
 #include <QFile>
 #include <ctime>
 #include "globalfuncs.h"
@@ -28,12 +29,19 @@ private:
     //-------------------
 
     class exceptionToLoop { };
+
+    QTimer* timer;
+    unsigned int s = 0;
+    unsigned int m = 0;
+
     QVector<QString> dontRepeatBase;
     QString dontRepeatNow;
     QString checkEnter;
     int count;
+
     void nextWord();
     int amountOfPulled(const QString);
+    void clarifyFontSize(QString&);
     void refreshTraining();
 
 private slots:
@@ -43,6 +51,7 @@ private slots:
     void on_pushButtonSkip_clicked();
     void on_pushButtonStartOver_clicked();
     void on_pushButtonSetImg_clicked();
+    void updateTime();
 };
 
 #endif // TRAININGMODE_H
